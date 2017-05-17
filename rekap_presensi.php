93f2cf2856e3edd02658aef32cde7078349dfcb8
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2017-05-17 17:19:31 +0700

    Filter Tampil lokasi dan foto

--- rekap_presensi.php
+++ rekap_presensi.php
@@ -34,6 +34,15 @@ include 'android/db.php';
 		    <input type="text" class="form-control datepicker" id="pwd" name="sampai_tanggal" placeholder="Sampai Tanggal">
 		  </div>
 
+		
+
+		  <div class="checkbox">
+  <label><input type="checkbox" value="1" name="tampil_foto">Tampil Foto</label>
+</div>
+	  <div class="checkbox">
+  <label><input type="checkbox" value="1" name="tampil_lokasi">Tampil Lokasi</label>
+</div>
+
 	  <button type="submit" class="btn btn-default">Cari</button>
 	</form>
 	<br>
@@ -44,10 +53,15 @@ include 'android/db.php';
 		<th>NIK</th>
 		<th>Waktu Masuk</th>
 		<th>Waktu Pulang</th>
-		<th>Lokasi Masuk</th>
-		<th>Lokasi Pulang</th>
+		<?php if (isset($_GET['tampil_lokasi'])): ?>
+			<th>Lokasi Masuk</th>
+		<th>Lokasi Pulang</th>	
+		<?php endif ?>
+	<?php if (isset($_GET['tampil_foto'])): ?>
 		<th>Foto Masuk</th>
 		<th>Foto Pulang</th>
+	<?php endif ?>
+		
 
 
 	</thead>
@@ -61,19 +75,27 @@ include 'android/db.php';
 		$dari_tanggal = tanggal_mysql($_GET['dari_tanggal']);
 		$sampai_tanggal = tanggal_mysql($_GET['sampai_tanggal']);
 
-		$query_presensi = $db->query("SELECT * FROM presensi WHERE tanggal_masuk >= '$dari_tanggal' AND tanggal_masuk <= '$sampai_tanggal'");
+		$query_presensi = $db->query("SELECT * FROM presensi WHERE nik = '$nik' AND  tanggal_masuk >= '$dari_tanggal' AND tanggal_masuk <= '$sampai_tanggal'");
 
 		while ($data_presensi = $query_presensi->fetch_array()) {
 			
 			echo "<tr>
 			<td>".$nik."</td>
 			<td>".$data_presensi['waktu_masuk']."</td>
-			<td>".$data_presensi['waktu_pulang']."</td>
-			<td>".$data_presensi['lokasi_absen_masuk']."</td>
-			<td>".$data_presensi['lokasi_absen_pulang']."</td>
-			<td><a href='android/".$data_presensi['gambar']."'> <img class='img-responsive' width='80px' height='80px' src='android/".$data_presensi['gambar']."'></a></td>
+			<td>".$data_presensi['waktu_pulang']."</td>";
+			if (isset($_GET['tampil_lokasi'])) {
+			
+	echo "<td>".$data_presensi['lokasi_absen_masuk']."</td>
+			<td>".$data_presensi['lokasi_absen_pulang']."</td>";
+			}
+				if (isset($_GET['tampil_foto'])) {
+						echo "<td><a href='android/".$data_presensi['gambar']."'> <img class='img-responsive' width='80px' height='80px' src='android/".$data_presensi['gambar']."'></a></td>
 			<td><a href='android/".$data_presensi['gambar_pulang']."'><img class='img-responsive'  width='80px' height='80px' src='android/".$data_presensi['gambar_pulang']."'</a></td>
-			</tr>";
+			";
+			}
+
+		
+			echo "</tr>";
 		}
 	}
 
@@ -82,8 +104,11 @@ include 'android/db.php';
 	 	</table>
 
 	 	</div>
+	 	<?php if (isset($_GET['nik'])): ?>
+	 		
+	 	<a href="export_rekap.php?nik=<?php echo $_GET['nik'] ?>&dari_tanggal=<?php echo $_GET['dari_tanggal'] ?>&sampai_tanggal=<?php echo $_GET['sampai_tanggal'] ?>" class="btn btn-success">Export Rekap Presensi</a>
 
-
+	 	<?php endif ?>
 
 </div>
 
